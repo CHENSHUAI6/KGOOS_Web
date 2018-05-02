05f4761f34bcdd39cf4ad51b7c7aefc5409a042b
CHENSHUAI6 <chenshuai@chenshuaideMacBook.local>
2018-05-02 16:31:03 +0800

    5-2
    界面

---
+++ KGOOS_Web/Controllers/ConTranController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KGOOS_Web.Controllers
+{
+    public class ConTranController : Controller
+    {
+        //
+        // GET: /ConTran/
+
+        public ActionResult MySelf()
+        {
+            return View();
+        }
+        public ActionResult InputGoods()
+        {
+            return View();
+        }
+        
+    }
+}
